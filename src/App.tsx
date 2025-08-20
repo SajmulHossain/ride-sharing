a39661723c67129410f27765f2068de86bdf01d7
Sajmul <sajmul1427@gmail.com>
2025-08-20 20:18:37 +0600

    initial setup and shadcn setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is App component</h1>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default App;
