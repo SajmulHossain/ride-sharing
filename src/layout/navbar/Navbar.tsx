ded3ca2f128f3741e9fbced884a0e5615720cce6
Sajmul <sajmul1427@gmail.com>
2025-08-20 21:23:05 +0600

    dark mode implemented

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -2,10 +2,11 @@ import { Button } from "@/components/ui/button";
 import { NavMenu } from "./nav-menu";
 import { NavigationSheet } from "./navigation-sheet";
 import Logo from "@/assets/logo/Logo";
+import { DarkModeToggler } from "@/components/DarkModeToggler";
 
 const Navbar = () => {
   return (
-    <div className="bg-muted">
+    <header className="bg-muted">
       <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
         <div className="h-full flex items-center justify-between mx-auto px-4">
          <Logo />
@@ -14,6 +15,7 @@ const Navbar = () => {
           <NavMenu className="hidden md:block" />
 
           <div className="flex items-center gap-3">
+            <DarkModeToggler />
             <Button
               variant="outline"
               className="hidden sm:inline-flex rounded-full"
@@ -21,7 +23,6 @@ const Navbar = () => {
               Sign In
             </Button>
             <Button className="rounded-full">Get Started</Button>
-
             {/* Mobile Menu */}
             <div className="md:hidden">
               <NavigationSheet />
@@ -29,7 +30,7 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-    </div>
+    </header>
   );
 };
 
