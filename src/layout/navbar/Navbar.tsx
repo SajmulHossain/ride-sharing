83888659f24d5dd1044e1f242b0009a4d16f7baf
Sajmul <sajmul1427@gmail.com>
2025-08-23 17:11:06 +0600

    conditional navbar design done

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -4,10 +4,11 @@ import { NavigationSheet } from "./navigation-sheet";
 import Logo from "@/assets/logo/Logo";
 import { DarkModeToggler } from "@/components/DarkModeToggler";
 import { Link } from "react-router";
-
-
+import { useGetMeQuery } from "@/redux/features/auth/auth.api";
 
 const Navbar = () => {
+  const { data: user } = useGetMeQuery(undefined);
+
   return (
     <header className="sticky top-0 inset-x-4 bg-background border dark:border-slate-700/70 z-50">
       <nav className="section py-3">
@@ -19,16 +20,24 @@ const Navbar = () => {
 
           <div className="flex items-center gap-3">
             <DarkModeToggler />
-            <Button
-              asChild
-              variant="outline"
-              className="hidden sm:inline-flex rounded-full"
-            >
-              <Link to="/login">Login</Link>
-            </Button>
-            <Button asChild className="rounded-full">
-              <Link to="/register">Register</Link>
-            </Button>
+            {user?.email ? (
+              <>
+                <Button
+                  asChild
+                  variant="outline"
+                  className="hidden sm:inline-flex rounded-full"
+                >
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button asChild className="rounded-full">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button variant="outline">Logout</Button>
+              </>
+            )}
             {/* Mobile Menu */}
             <div className="md:hidden">
               <NavigationSheet />
