8b0d268bc6e0f4d755a507c194254dd2e8daf003
Sajmul <sajmul1427@gmail.com>
2025-09-06 13:34:13 +0600

    active status toggle with backend fixed

--- src/components/DriverActiveToggler.tsx
+++ src/components/DriverActiveToggler.tsx
@@ -1,24 +1,37 @@
-import { useId } from "react";
+import { useEffect, useId, useState } from "react";
 
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { useGetActiveStatusQuery } from "@/redux/features/driver/driver.api";
+import { useGetActiveStatusQuery, useToggleStatusMutation } from "@/redux/features/driver/driver.api";
 
 export default function ActiveToggler() {
   const id = useId();
+  const [checked, setChecked] = useState(false);
   const {data} = useGetActiveStatusQuery(undefined);
-  const handleActiveStatus = () => {
-    
+  const [toggleStatus] = useToggleStatusMutation();
+  const handleActiveStatus = async () => {
+    setChecked(val => !val);
+    try {
+      await toggleStatus(undefined).unwrap();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err :unknown) {
+      setChecked(!checked);
+    }
   }
+
+  useEffect(() => {
+    setChecked(data || false);
+  },[data])
+
 
   return (
     <div className="inline-flex items-center gap-2">
       <Label htmlFor={id} className="text-sm font-medium">
-        {data.data ? "ACTIVE" : "INACTIVE"}
+        {data ? "ACTIVE" : "INACTIVE"}
       </Label>
       <Switch
         id={id}
-        // checked={data.data}
+        checked={checked}
         onCheckedChange={handleActiveStatus}
         aria-label="Toggle switch"
       />
--- src/redux/features/driver/driver.api.ts
+++ src/redux/features/driver/driver.api.ts
@@ -1,14 +1,22 @@
 import { baseApi } from "@/redux/baseApi";
+import type { Response } from "@/types";
 
 export const driverApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getActiveStatus: builder.query({
+    getActiveStatus: builder.query<boolean, unknown>({
       query: () => ({
         url: "/drivers/status",
         method: "GET",
       }),
+      transformResponse: (res: Response<boolean>) => res.data
     }),
+    toggleStatus: builder.mutation({
+        query: () => ({
+            url: "/drivers/active",
+            method: "PATCH",
+        })
+    })
   }),
 });
 
-export const { useGetActiveStatusQuery } = driverApi;
+export const { useGetActiveStatusQuery, useToggleStatusMutation } = driverApi;
